f887da0cdf7bcc0c88f6642ba0e03cda87b26995
Herzlia Jane Barangan <herzliajane.barangan@biotechfarms.net>
2022-08-01 15:45:10 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
+
+const app = express();
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.use(logger("dev"));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Meow!" });
+});
+
+// Set port, listen for requests
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
