2e8a8ae617072588731dd3744a95a3ed4a53cc33
Herzlia Jane Barangan <herzliajane.barangan@biotechfarms.net>
2022-08-04 17:33:57 +0800

    added jest package

--- test/item.test.js
+++ test/item.test.js
@@ -72,5 +72,52 @@ describe("Items API", () => {
     });
   });
 
+//PUT METHOD
+  describe("Test PUT route", () => {
+    it("it should update an item", (done) => {
+      const barcode = "7342433";
+      const item = {
+        barcode: Math.floor(Math.random() * 10000) + 99999,
+        name: "mang juan",
+        quantity: Math.floor(Math.random() * 50) + 1,
+        cost: Math.floor(Math.random() * 50) + 300,
+      };
+
+      chai
+        .request(API)
+        .put("/items/edit" + barcode)
+        .send(item)
+        .end((err, res) => {
+          res.should.have.status(200);
+          // res.body.should.be.a("object");
+          // res.body.should.have.property("Items Created Successfully");
+          // res.body.should.have.property("name");
+          // res.body.should.have.property("quantity");
+          // res.body.should.have.property("cost");
+          // res.body.should.have.property("barcode");
+          done();
+        });
+    });
+  });
+
+  //another assertion for DELETE/:id
+  describe("Test DELETE/:id route", () => {
+    it("it should delete an item by the given id", (done) => {
+      const barcode = "7342433";
+      chai
+        .request(API)
+        .get(`/items/delete/${barcode}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          // res.body.should.be.a("object");
+          // res.body.should.have.property("name");
+          // res.body.should.have.property("quantity");
+          // res.body.should.have.property("cost");
+          // res.body.should.have.property("id");
+          // res.body.should.have.property("barcode");
+          done();
+        });
+    });
+  });
   
 });
